620195ff7b13caa3fb1502e1d23fb61f018608ba
Kris4ou5 <krisiivanov08@gmail.com>
2026-03-29 16:27:12 +0300

    клас за инспекция ,който работи с щетите имплементиран.

--- Domain/RentalObjects/Inspections.cs
+++ Domain/RentalObjects/Inspections.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarRentalSys.Domain.Enums;
 
 namespace CarRentalSys.Domain.RentalObjects
 {
-    internal class Inspections
+    public class Inspections
     {
+        public int Id { get; private set; }
+        public int RentalID { get; private set; }
+        public List<Damage> Damages { get; private set; } = new List<Damage>();
+        public decimal TotalCost { get; private set; }
+
+        public Inspections(int id, int rentalid, List<Damage> damages) 
+        {  
+          Id = id;
+          RentalID = rentalid;
+          Damages = damages;
+          foreach(var dmg  in Damages)
+          {
+                TotalCost += dmg.damageCost;
+          }
+        }
+
+
     }
 }
