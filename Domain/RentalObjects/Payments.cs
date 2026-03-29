e8abc36e7872aaef3b38f70046c550cc0357c87e
Kris4ou5 <krisiivanov08@gmail.com>
2026-03-29 16:27:30 +0300

    Клас за плащания имплементиран.

--- Domain/RentalObjects/Payments.cs
+++ Domain/RentalObjects/Payments.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarRentalSys.Domain.Enums;
 
 namespace CarRentalSys.Domain.RentalObjects
 {
-    internal class Payments
+    public class Payments
     {
+        public int Id { get; private set; }
+        public int RentalId { get; private set; }
+        public CarCategory CarCategory { get; private set; }
+        public decimal Amount { get; private set; }
+
+         public Payments(int id, int rentalId, CarCategory category, int amount)
+         {
+            Id = id;
+            RentalId = rentalId;
+            CarCategory = category;
+            Amount = amount;
+        }
+
+
+
     }
 }
