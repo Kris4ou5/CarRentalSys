c5a770c9a985f186dd7768d9355fbd34d74cdea1
Kris4ou5 <krisiivanov08@gmail.com>
2026-03-30 21:11:00 +0300

    Created Implemented Storage

---
+++ Infrastructure/CarRentalStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRentalSys.Domain.Enums;
+using CarRentalSys.Domain.Entities;
+using CarRentalSys.Domain.RentalObjects;
+
+namespace CarRentalSys.Infrastructure
+{
+    public class CarRentalStorage
+    {
+        public int NextID { get; set; } = 1;
+
+        public List<Car> AllCars  = new List<Car>();
+        public List<Rentals> Rentals = new List<Rentals>();
+        public List<Customer> Customers = new List<Customer>();
+    }
+}
