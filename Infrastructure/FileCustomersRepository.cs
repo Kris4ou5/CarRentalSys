2858ffb91c91e7609a49eabdabf5d47d735269bd
plamenivn <palmenivanov768@gmail.com>
2026-04-03 21:33:34 +0300

    Infrastructure class FileCustomersRepository - public FileCustomersRepository and  public IReadOnlyList

--- Infrastructure/FileCustomersRepository.cs
+++ Infrastructure/FileCustomersRepository.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarRentalSys.Domain.Entities;
 
 namespace CarRentalSys.Infrastructure
 {
-    internal class FileCustomersRepository
+    public class FileCustomersRepository
     {
+        private readonly FileStorage _storage;
 
+        public FileCustomersRepository(FileStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public IReadOnlyList<Customer> GetAll()
+        {
+            var saved = _storage.Load();
+            return saved.Customers;
+        }
     }
 }
