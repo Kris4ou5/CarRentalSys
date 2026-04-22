65c9cd00b18c50677d85b654cf4ea32affa4014d
kris <PCK214@K214>
2026-04-22 10:13:15 +0300

    Made Services

--- Application/Services/CustomerService.cs
+++ Application/Services/CustomerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarRentalSys.Application.Interfaces;
+using CarRentalSys.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,51 @@ using System.Threading.Tasks;
 
 namespace CarRentalSys.Application.Services
 {
-    internal class CustomerService
+    public class CustomerService
     {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IRentalRepository _rentalRepository;
+        public CustomerService(ICustomerRepository customerRepository, IRentalRepository rentalRepo)
+        {
+            this._customerRepository = customerRepository;
+            this._rentalRepository = rentalRepo;
+        }
+
+        public void RegisterCustomer(string name, string driveLcs, string phone, string email)
+        {
+            var newCustomer = new Customer(0, name, driveLcs, phone, email);
+            ValidateCustomer(newCustomer);
+            _customerRepository.Save(newCustomer);
+        }
+
+        public void EditCustomer(Customer customer)
+        {
+            ValidateCustomer(customer);
+            _customerRepository.Save(customer);
+        }
+
+        public IReadOnlyList<Rentals> GetCustomerRentalHistory(int customerid)
+        {
+            var customer = _customerRepository.GetById(customerid);
+            ValidateCustomer(customer);
+            var rentals = _rentalRepository.GetAll();
+            List<Rentals> customerRentals = new List<Rentals>();
+            foreach(var rental in rentals)
+            {
+                if(rental.CustomerId == customer.Id)
+                {
+                    customerRentals.Add(rental);
+                }
+            }
+            return customerRentals;
+        }
+
+        private void ValidateCustomer(Customer customer)
+        {
+            if (customer == null) throw new ArgumentException("Клиент не валиден");
+            if (string.IsNullOrEmpty(customer.FullName)) throw new ArgumentException("Невалидно име.");
+            if (string.IsNullOrEmpty(customer.DriverLicenseNumber) || customer.DriverLicenseNumber.Length < 9 || customer.DriverLicenseNumber.Length > 9) throw new ArgumentException("Невалиден номер на книжка.");
+            if (string.IsNullOrEmpty(customer.Phone)) throw new ArgumentException("Невалиден телефонен номер.");
+        }
     }
 }
