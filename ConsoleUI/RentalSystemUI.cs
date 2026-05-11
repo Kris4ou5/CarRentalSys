8f1340e5d4a16b86869cd5060a3b41c98ea75211
plamenivn <palmenivanov768@gmail.com>
2026-05-11 21:21:06 +0300

    RentalSystemUI class - public RentalSystemUI

--- ConsoleUI/RentalSystemUI.cs
+++ ConsoleUI/RentalSystemUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarRentalSys.Application.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,17 @@ namespace CarRentalSys.ConsoleUI
 {
     internal class RentalSystemUI
     {
+        private readonly CarService _carService;
+        private readonly CustomerService _customerService;
+        private readonly RentalService _rentalService;
 
+        public RentalSystemUI(CarService carService, CustomerService customerService, RentalService rentalService)
+        {
+            _carService = carService;
+            _customerService = customerService;
+            _rentalService = rentalService;
+        }
+
+       
     }
 }
