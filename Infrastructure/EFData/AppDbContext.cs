857c34f62ed3a340053823b8476d1b64504c7002
Kris4ou5 <krisiivanov08@gmail.com>
2026-05-20 23:53:53 +0300

    Started ORM preparation

---
+++ Infrastructure/EFData/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalSys.Infrastructure.EFData
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext() { }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-BLQDC9N;Database=WalletDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+
+
+    }
+}
